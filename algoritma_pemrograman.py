7abb3772ea4d80b2287ec63258f2a37a0d33638a
wafi <zain.wafim@gmail.com>
2025-10-23 05:27:50 +0700

    fix code

--- algoritma_pemrograman.py
+++ algoritma_pemrograman.py
@@ -1,40 +1,40 @@
 # TIPE DATA
-#nama = 'ikan'                       #string
-#umur = 50 ,str ("tahun")            #integer
-#tinggi = 175.5                      #float 
-#aktif = True                        #boolean
-#print(nama,umur,tinggi,aktif)        
+nama = 'ikan'                       #string
+umur = 50 ,str ("tahun")            #integer
+tinggi = 175.5                      #float 
+aktif = True                        #boolean
+print(nama,umur,tinggi,aktif)        
 
 
 # OPERASI ARITMATIKA
-# angka1 = int(input("Masukkan angka pertama : "))
-# angka2 = int(input("Masukkan angka kedua : "))
+angka1 = int(input("Masukkan angka pertama : "))
+angka2 = int(input("Masukkan angka kedua : "))
 
-# hasil_jumlah = angka1 + angka2
-# hasil_kurang = angka1 - angka2
-# hasil_kali = angka1 * angka2
-# hasil_bagi = angka1 / angka2
-# hasil_bagibulat = angka1 // angka2
+hasil_jumlah = angka1 + angka2
+hasil_kurang = angka1 - angka2
+hasil_kali = angka1 * angka2
+hasil_bagi = angka1 / angka2
+hasil_bagibulat = angka1 // angka2
 
-# print("Hasil Penjumlahan : ",hasil_jumlah)
-# print("Hasil Pengurangan : ",hasil_kurang)
-# print("Hasil Perkalian : ",hasil_kali)
-# print("Hasil Pembagian : ",hasil_bagi)
-# print("Hasil Pembagian Bulat : ",hasil_bagibulat)
+print("Hasil Penjumlahan : ",hasil_jumlah)
+print("Hasil Pengurangan : ",hasil_kurang)
+print("Hasil Perkalian : ",hasil_kali)
+print("Hasil Pembagian : ",hasil_bagi)
+print("Hasil Pembagian Bulat : ",hasil_bagibulat)
 
 # MENGHITUNG LUAS PERSEGI PANJANG
-# print("Menghitung Luas Persegi Panjang")
-# panjang = int(input("Masukkan panjang : "))
-# lebar = int(input("Masukkan lebar : "))
-# luas = panjang * lebar
-# print("Luas Persegi Panjang adalah : ",luas)
+print("Menghitung Luas Persegi Panjang")
+panjang = int(input("Masukkan panjang : "))
+lebar = int(input("Masukkan lebar : "))
+luas = panjang * lebar
+print("Luas Persegi Panjang adalah : ",luas)
 
 # # MENGHITUNG RATA-RATA DUA ANGKA
-# print("Menghitung Rata-Rata Dua Angka")
-# angka1 = int(input("Masukkan angka pertama : "))    
-# angka2 = int(input("Masukkan angka kedua : "))
-# rata_rata = (angka1 + angka2) / 2
-# print("Rata-rata dari dua angka adalah : ",rata_rata)
+print("Menghitung Rata-Rata Dua Angka")
+angka1 = int(input("Masukkan angka pertama : "))    
+angka2 = int(input("Masukkan angka kedua : "))
+rata_rata = (angka1 + angka2) / 2
+print("Rata-rata dari dua angka adalah : ",rata_rata)
 
 # ALGORITMA MENGHITUNG LUAS PERSEGI PANJANG
 # 1. MULAI
@@ -51,36 +51,36 @@
 # 5. SELESAI
 
 # LEVEL 1 MENGHITUNG LUAS LINGKARAN
-# print("Menghitung Luas Lingkaran")
-# phi = 3.14
-# jari_jari = int(input("Masukkan jari-jari : "))
-# luas_lingkaran = phi * jari_jari * jari_jari
-# print("Luas Lingkaran adalah : ",luas_lingkaran)
+print("Menghitung Luas Lingkaran")
+phi = 3.14
+jari_jari = int(input("Masukkan jari-jari : "))
+luas_lingkaran = phi * jari_jari * jari_jari
+print("Luas Lingkaran adalah : ",luas_lingkaran)
 
 
 # # LEVEL 2 
-# nilai1 = int(input("Masukkan nilai pertama : "))
-# nilai2 = int(input("Masukkan nilai kedua : "))
-# nilai3 = int(input("Masukkan nilai ketiga : "))
-# nilai4 = int(input("Masukkan nilai keempat : "))
-# nilai5 = int(input("Masukkan nilai kelima : "))
-# rata_rata = (nilai1 + nilai2 + nilai3 + nilai4 + nilai5) / 5
-# if rata_rata >= 75 :
-#     print("Selamat, Anda Lulus!")
-# else :
-#     print("Maaf, Anda Tidak Lulus!")
-# print("Rata-rata nilai Anda adalah : ",rata_rata)
+nilai1 = int(input("Masukkan nilai pertama : "))
+nilai2 = int(input("Masukkan nilai kedua : "))
+nilai3 = int(input("Masukkan nilai ketiga : "))
+nilai4 = int(input("Masukkan nilai keempat : "))
+nilai5 = int(input("Masukkan nilai kelima : "))
+rata_rata = (nilai1 + nilai2 + nilai3 + nilai4 + nilai5) / 5
+if rata_rata >= 75 :
+    print("Selamat, Anda Lulus!")
+else :
+    print("Maaf, Anda Tidak Lulus!")
+print("Rata-rata nilai Anda adalah : ",rata_rata)
 
 # LEVEL 3
-# jam_parkir = int(input("Masukkan jam parkir : "))
-# if jam_parkir <= 2 :
-#     biaya_parkir = 5000
-# else :
-#     biaya_parkir = 5000 + (jam_parkir - 2) * 3000
-#     if biaya_parkir > 20000 :
-#         diskon = 0.15 * biaya_parkir,
-#         biaya_parkir = biaya_parkir - 0.15 * biaya_parkir
-# print("Biaya parkir yang harus dibayar adalah : ",biaya_parkir)
+jam_parkir = int(input("Masukkan jam parkir : "))
+if jam_parkir <= 2 :
+    biaya_parkir = 5000
+else :
+    biaya_parkir = 5000 + (jam_parkir - 2) * 3000
+    if biaya_parkir > 20000 :
+        diskon = 0.15 * biaya_parkir,
+        biaya_parkir = biaya_parkir - 0.15 * biaya_parkir
+print("Biaya parkir yang harus dibayar adalah : ",biaya_parkir)
 
 
 
